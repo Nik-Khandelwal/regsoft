d2c2b38bc45790da5c16f081e37a91ae0bb4ac4b
piyalimanna <piyalimanna1997@gmail.com>
2018-08-30 05:36:06 +0530

    Update urls.py

--- register/urls.py
+++ register/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
 	#url(r'^add/(?P<number>[0-9]+)/error/$',error),
 	url(r'^instructions/$', instructions),
 	url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',activate, name='activate'),
+	url(r'^paytm/payment/', getpay),
+    	url(r'^paytm/response/', response),
 	
 	
 ]

553177f4aff324a53a9018f3af5ad8d11eeefbc5
Arpit Anshuman <f2016250@pilani.bits-pilani.ac.in>
2018-09-03 17:14:51 +0530

    Models Tab

--- main/models.py
+++ main/models.py
@@ -187,20 +187,20 @@ class Amounts(models.Model):
 		return self.name
 
 class PaytmHistory(models.Model):
-    user = models.IntegerField()
-    ORDERID = models.CharField('ORDER ID', max_length=30)
-    TXNDATE = models.DateTimeField('TXN DATE', default=timezone.now)
-    TXNID = models.IntegerField('TXN ID')
-    BANKTXNID = models.IntegerField('BANK TXN ID', null=True, blank=True)
-    BANKNAME = models.CharField('BANK NAME', max_length=50, null=True, blank=True)
-    RESPCODE = models.IntegerField('RESP CODE')
-    PAYMENTMODE = models.CharField('PAYMENT MODE', max_length=10, null=True, blank=True)
-    CURRENCY = models.CharField('CURRENCY', max_length=4, null=True, blank=True)
-    GATEWAYNAME = models.CharField("GATEWAY NAME", max_length=30, null=True, blank=True)
-    MID = models.CharField(max_length=40)
-    RESPMSG = models.TextField('RESP MSG', max_length=250)
-    TXNAMOUNT = models.FloatField('TXN AMOUNT')
-    STATUS = models.CharField('STATUS', max_length=12)
-    def __str__(self):
+	user = models.IntegerField()
+	ORDERID = models.CharField('ORDER ID', max_length=30)
+	TXNDATE = models.DateTimeField('TXN DATE', default=timezone.now)
+	TXNID = models.IntegerField('TXN ID')
+	BANKTXNID = models.IntegerField('BANK TXN ID', null=True, blank=True)
+	BANKNAME = models.CharField('BANK NAME', max_length=50, null=True, blank=True)
+	RESPCODE = models.IntegerField('RESP CODE')
+	PAYMENTMODE = models.CharField('PAYMENT MODE', max_length=10, null=True, blank=True)
+	CURRENCY = models.CharField('CURRENCY', max_length=4, null=True, blank=True)
+	GATEWAYNAME = models.CharField("GATEWAY NAME", max_length=30, null=True, blank=True)
+	MID = models.CharField(max_length=40)
+	RESPMSG = models.TextField('RESP MSG', max_length=250)
+	TXNAMOUNT = models.FloatField('TXN AMOUNT')
+	STATUS = models.CharField('STATUS', max_length=12)
+	def __str__(self):
 		return self.ORDERID+','+str(self.TXNID)
 

4062da0d3b8c3bedefeed68433bda8daad255a5b
Arpit Anshuman <f2016250@pilani.bits-pilani.ac.in>
2018-12-31 15:18:30 +0530

    Update modify_emails.py

--- modify_emails.py
+++ modify_emails.py
@@ -1,5 +1,13 @@
 from main import models
 
+for player in models.CustomUser.objects.exclude(email=''):
+    tempemail = player.email
+    print (tempemail)
+    tempemail +='xyz'
+    player.email = tempemail
+    player.save()
+    print (tempemail)
+
 for player in models.Regplayer.objects.exclude(email_id=None):
     tempemail = player.email_id
     print (tempemail)
